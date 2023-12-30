25dc98b50af08c4f4e9ef31d8f4a00b61a5e92ec
YJS <y.j3ms.n@gmail.com>
2023-12-31 08:56:32 +0900

    feat: enable testing w/ files

--- Main.hs
+++ Main.hs
@@ -1,4 +1,23 @@
 module Main where
 
+import Ast
+import Parser
+import System.Environment
+
+parseFile :: Parser a -> FilePath -> IO (Maybe a)
+parseFile parser file = do
+  input <- readFile file
+  return (snd <$> runParser parser input)
+
+printf :: (Show a) => a -> IO ()
+printf content = do
+  print content
+  putStrLn "----------------------------------------"
+
 main :: IO ()
-main = print "Welcome to hason!"
+main = do
+  args <- getArgs
+  maybeJsonValues <- mapM (parseFile jsonParser) args
+  case sequence maybeJsonValues of
+    Just jsonValues -> mapM_ printf jsonValues
+    Nothing -> print "Error while parsing"
